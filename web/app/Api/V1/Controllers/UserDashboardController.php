1a6bade6f027a7f76268e971506cd9e7e36e8cd3
Donald Blessing <donaldblessing9@gmail.com>
2022-06-03 08:32:10 +0100

    Corrected swagger error

--- web/app/Api/V1/Controllers/UserDashboardController.php
+++ web/app/Api/V1/Controllers/UserDashboardController.php
@@ -7,7 +7,6 @@ use App\Traits\SubscribersAnalysisTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
-use OpenApi\Annotations as OA;
 use Throwable;
 
 class UserDashboardController extends Controller
